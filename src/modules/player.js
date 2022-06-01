4196818cd3c785c1b5d70449d2db1a328ed31e02
Victor Avila <victoravila1997@gmail.com>
2022-05-31 21:39:07 -0500

    Completed initial tests for factory functions

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,7 +1,22 @@
 const Player = (name) => {
-  const attack = (posX, posY) => [posX, posY];
+  const attack = (posX, posY, board) => {
+    if (posX > 9 || posY > 9) { return null; }
+    if (board[posY][posX] === '') {
+      return [posX, posY];
+    }
+    return null;
+  };
 
-  return { attack };
+  const randomAttack = (board) => {
+    const posX = Math.floor(Math.random() * 10);
+    const posY = Math.floor(Math.random() * 10);
+    if (board[posY][posX] === '') {
+      return [posX, posY];
+    }
+    return null;
+  };
+
+  return { attack, randomAttack };
 };
 
 export default Player;
--- src/tests/player.test.js
+++ src/tests/player.test.js
@@ -1,7 +1,36 @@
 import Player from '../modules/player';
+import Gameboard from '../modules/gameboard';
 
 const player1 = Player('player1');
+const computer = Player('computer');
+const game = Gameboard();
+
+beforeEach(() => {
+  jest.spyOn(global.Math, 'random').mockReturnValue(0);
+});
+
+afterEach(() => {
+  jest.spyOn(global.Math, 'random').mockRestore();
+});
 
 test('Return coordinates', () => {
-  expect(player1.attack(1, 1)).toEqual([1, 1]);
+  expect(player1.attack(1, 1, game.board)).toEqual([1, 1]);
+});
+
+test('Cant attack same location twice', () => {
+  game.receiveAttack(1, 1);
+  expect(player1.attack(1, 1, game.board)).toBe(null);
+});
+
+test('Cant attack outside of board', () => {
+  expect(player1.attack(10, 10, game.board)).toBe(null);
+});
+
+test('Computer makes random attack', () => {
+  expect(computer.randomAttack(game.board)).toEqual([0, 0]);
+});
+
+test('Computer does not attack same location twice', () => {
+  game.receiveAttack(0, 0);
+  expect(computer.randomAttack(game.board)).toBe(null);
 });
